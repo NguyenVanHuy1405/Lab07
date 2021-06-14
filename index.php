2ed9b6cca5eea40fb516bf791085fadb8ed75bba
NguyenVanHuy1405 <84852270+NguyenVanHuy1405@users.noreply.github.com>
2021-06-14 09:58:05 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<tittle>WElcome to PHP for cloud app!</tittle>
+</head>
+<body>
+<?php
+echo "<h1> Hello cloud computing class</h1>";
+for( $i=1; $i<=100; $i++ )
+{
+echo $i;
+echo "<br>";
+}
+?>
+</body>
+</html>
