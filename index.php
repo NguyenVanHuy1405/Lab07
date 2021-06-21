044dfe11813f9950755fb0eae2c3b7340bb96ad1
NguyenVanHuy1405 <84852270+NguyenVanHuy1405@users.noreply.github.com>
2021-06-22 01:08:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Select Dropdown Example in PHP</title>
   <style>
     .container {
       max-width: 350px;
@@ -87,10 +84,12 @@
       <div class="select-block">
         <select name="Fruit">
           <option value="" disabled selected>Choose option</option>
-          <option value="shop1">shop1</option>
-          <option value="shop2">shop2</option>
-          <option value="All_shop">All_shop</option>
-          
+          <option value="Apple">Apple</option>
+          <option value="Banana">Banana</option>
+          <option value="Coconut">Coconut</option>
+          <option value="Blueberry">Blueberry</option>
+          <option value="Strawberry">Strawberry</option>
+        </select>
         <div class="selectIcon">
           <svg focusable="false" viewBox="0 0 104 128" width="25" height="35" class="icon">
             <path d="m2e1 95a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm14 55h68v1e1h-68zm0-3e1h68v1e1h-68zm0-3e1h68v1e1h-68z"></path>
@@ -98,26 +97,15 @@
         </div>
       </div>
 
-      <input type="submit" name="submit" value="Choose options">
+      <input type="submit" name="submit" value ="Choose options">
     </form>
 
     <?php
       if(isset($_POST['submit'])){
         if(!empty($_POST['Fruit'])) {
-          $selected = $_POST['Fruit'];}
-          if ($selected =="shop1")
-          {
-            echo "select shop1");
-          }
-        if ($selected =="shop2")
-        {
-        echo "select shop2");
-        }  
-        if($selected =="All_shop") 
-        {
-        echo" select two shop";
-        }
-        else {
+          $selected = $_POST['Fruit'];
+          echo 'You have chosen: ' . $selected;
+        } else {
           echo 'Please select the value.';
         }
       }
