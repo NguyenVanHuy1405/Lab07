058159e23c1790e2e850fd8fab1e36d9ba5883b2
NguyenVanHuy1405 <84852270+NguyenVanHuy1405@users.noreply.github.com>
2021-06-22 08:22:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -84,11 +84,9 @@
       <div class="select-block">
         <select name="Fruit">
           <option value="" disabled selected>Choose option</option>
-          <option value="Apple">Apple</option>
-          <option value="Banana">Banana</option>
-          <option value="Coconut">Coconut</option>
-          <option value="Blueberry">Blueberry</option>
-          <option value="Strawberry">Strawberry</option>
+          <option value="shop1">shop1</option>
+          <option value="shop2">shop2</option>
+          <option value="all">all</option>
         </select>
         <div class="selectIcon">
           <svg focusable="false" viewBox="0 0 104 128" width="25" height="35" class="icon">
